dff47678a46671c1083cd261c6b8bafae16488f2
Michael Meola <meolaadonai@gmail.com>
2023-11-03 13:13:19 -0600

    arrays finished

--- src/arrays.js
+++ src/arrays.js
@@ -3,6 +3,7 @@
 //   greaterThanTen([1, 2, 3, 11, 12, 13]);
 //   => [11, 12, 13]
 function greaterThanTen(numbers) {
+
   const bigNumbers = []
 
   for (let i = 0; i < numbers.length; i++){
@@ -18,26 +19,65 @@ function greaterThanTen(numbers) {
 // Ex.:
 //   bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
 //   => ['banana', 'Bonobo]
-function bWords(words) {}
+function bWords(words){
+  
+  const result = []
+
+  for(let i = 0; i < words.length; i++){
+    const word = words[i]
+
+    if(word[0].toUpperCase() === 'B'){
+      result.push(word)
+    }
+  }
+
+  return result
+}
 
 // Add all the elements from additionalItems to the end of originalArray.
 // Return the originalArray..
 // Ex.:
 //  extend([1, 2, 3], [4, 5, 6]);
 //  => [1, 2, 3, 4, 5, 6]
-function extend(originalArray, additionalItems) {}
+function extend(originalArray, additionalItems){
+  
+  for(const element of additionalItems)
+  originalArray.push(element)
+
+  return originalArray
+}
 
 // Return an array of all items with the given length.
 // Ex.:
 //   itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3);
 //   => ['bbb', 'eee']
-function itemsWithLength(items, length) {}
+function itemsWithLength(items, length){
+  
+  const newArray = []
+
+  for(let i = 0; i < items.length; i++){
+    if(items[i].length === length){
+      newArray.push(items[i])
+    }
+  }
+
+  return newArray
+}
 
 // Return an array with every other element from the input array (start with index 0).
 // Ex.:
 //   everyOtherItem(['a', 'b', 'c', 'd', 'e']);
 //   => ['a', 'c', 'e']
-function everyOtherItem(items) {}
+function everyOtherItem(items){
+  
+  const newArray = []
+
+  for(let i = 0; i < items.length; i += 2){
+    newArray.push(items[i])
+  }
+
+  return newArray
+}
 
 // Given a list of words and a letter, return the indexes of the words that
 // start with that letter. You can assume that the words and letter will always
@@ -45,7 +85,21 @@ function everyOtherItem(items) {}
 // Ex.:
 //   findWordsStartingWith(['apple', 'banana', 'kiwi', 'pear', 'bacon'], 'b');
 //   => [1, 4]
-function findWordsStartingWith(words, letter) {}
+function findWordsStartingWith(words, letter){
+
+  const newArray = []
+
+  for(let i = 0; i < words.length; i++){
+    
+    const word = words[i]
+    
+    if(word.startsWith(letter)){
+      newArray.push(i)
+    }
+  }
+
+  return newArray
+}
 
 // Return the `n` smallest values in the array in descending order (largest
 // numbers first). Assume that `n` will always be less than the length of the
@@ -53,7 +107,27 @@ function findWordsStartingWith(words, letter) {}
 // Ex.:
 //   smallestNItems([1, 30, 4, 21, 100000], 3);
 //   => [21, 4, 1]
-function smallestNItems(items, n) {}
+function smallestNItems(items, n){
+  
+  const newArray = []
+
+  for(let i = 0; i < n; i++){
+    let smallest = items[0]
+    let smallestIndex = 0
+
+    for(let x = 1; x < items.length; x++){
+      if(items[x] < smallest){
+        smallest = items[x]
+        smallestIndex = x
+      }
+    }
+
+    newArray.unshift(smallest)
+    items.splice(smallestIndex, 1)
+  }
+
+  return newArray
+}
 
 // Search for a value in the array and return its index. If the value appears
 // more than once, return the index of the *FIRST* occurrence of the value. If
@@ -61,14 +135,30 @@ function smallestNItems(items, n) {}
 // Ex.:
 //   findIndex(['a', 'b', 'c', 'a', 'b', 'c'], 'c');
 //   => 2
-function findIndex(items, value) {}
+function findIndex(items, value){
+  for(let i = 0; i < items.length; i++){
+    if (items[i] === value){
+      return (i)
+    }
+  }
+  return undefined
+}
 
 // Given a start number and stop number, return a new array containing all the numbers
 // between the start and stop number.
 // Ex.:
 //   range(1, 5);
 //   => [1, 2, 3, 4, 5]
-function range(start, stop) {}
+function range(start, stop){
+  
+  const result = []
+
+  for(let i = start; i <= stop; i++){
+    result.push(i)
+  }
+
+  return result
+}
 
 export {
   bWords,
